27b590014ef143b7f5eb836a9427c31bee624bde
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-01-21 20:04:24 +0800

    0121

---
+++ app/src/main/java/com/mozhimen/debugk/test/DebugKApplication.kt
@@ -0,0 +1,19 @@
+package com.mozhimen.debugk.test
+
+import com.mozhimen.basick.elemk.android.app.bases.BaseApplication
+import com.mozhimen.basick.lintk.optin.OptInApiMultiDex_InApplication
+
+/**
+ * @ClassName DebugKApplication
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2022/12/16 14:51
+ * @Version 1.0
+ */
+@OptIn(OptInApiMultiDex_InApplication::class)
+class DebugKApplication : BaseApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
